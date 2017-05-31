3f9bc2e75c10466f240a425962470493d0db2395
Mohamad Eldhemy <m.dhemy@outlook.com>
2017-06-01 01:51:49 +0200

    Initial commit

---
+++ app/Beak/Response.php
@@ -0,0 +1,202 @@
+<?php
+/**
+* @author Dhemy
+* @contact m.dhemy@outlook.com
+* 
+* Implements HTTP status reponses with status codes
+**/
+namespace App\Beak;
+Class Response{
+
+	private $headers;
+	private $cookies;
+	private $data;
+	private $code;
+	private $response;
+
+	public function __construct(){
+		$this->headers = [];
+		$this->cookies = [];
+		$this->response = collect([]);
+		$this->code = 200;
+	}
+
+	/**
+	 * puts the status code within the response collection
+	 * @return void
+	 */
+	
+	private function putCode(){
+		$this->response->put('status_code', $this->code);
+	}
+
+	/**
+	 * puts data witin the response collection
+	 * @return void
+	 */
+	
+	private function putData(){
+		$this->response->put('data', $this->data);
+	}
+
+	/**
+	 * defines a cookie to be sent along with the rest of the HTTP headers
+	 * @param  [string]  $name     The name of the cookie.
+	 * @param  [string]  $value    The value of the cookie
+	 * @param  integer $expire   The time the cookie expires. 
+	 * @param  string  $path     The path on the server in which the cookie will be available on.
+	 * @param  string  $domain   The (sub)domain that the cookie is available to.
+	 * @param  boolean $secure   secure HTTPS connection or HTTP
+	 * @param  boolean $httponly When TRUE the cookie will be made accessible only through the HTTP protocol.
+	 * @return  this
+	 */
+	
+	public function withCookie($name, 
+			$value, 
+			$expire = 0, 
+			$path = '/', 
+			$domain = "", 
+			$secure = false, 
+			$httponly = false){
+		setcookie($name, $value, $expire, $path, $domain, $secure, $httponly);
+		return $this;
+	}
+
+	/**
+	 * Sets a cookie with the name token and the given value , it's HTTPOnly
+	 * @param  [string]  $value    The value of the cookie
+	 * @param  @param  integer $expire   The time the cookie expires. 
+	 * @param  string  $path     The path on the server in which the cookie will be available on.
+	 * @param  string  $domain   The (sub)domain that the cookie is available to.
+	 * @param  boolean $secure   secure HTTPS connection or HTTP
+	 * @return this
+	 */
+	
+	public function withTokenCookie($value, $expire = 0, $path = "/", $domain = "", $secure=false){
+		$this->withCookie('token', $value, $expire, $path, $domain, $secure, true);
+		return $this;
+	}
+
+	/**
+	 *  you may use the header method to add a series of headers 
+	 *  to the response before sending it back to the user
+	 * @param  [string] $name  [header name]
+	 * @param  [string] $value [header value]
+	 * @return this
+	 */
+	
+	public function withHeader($name, $value){
+		$this->headers[$name] = $value;
+		return $this;
+	}
+
+	/**
+	 * you may use the withHeaders method to specify an array of headers to be added to the response
+	 * @param  [array] $headers 
+	 * @return this
+	 */
+	
+	public function withHeaders($headers){
+		$this->headers = array_merge($this->headers, $headers);
+		return $this;
+	}
+
+	/**
+	 * Trigger method
+	 * @param   $data response data
+	 * @param  integer $code     status code
+	 * @return illuminate json response
+	 */
+	
+	public function respond(){
+		$this->putCode();
+		$this->putData();
+		return response()->json(
+				$this->response, 
+				$this->code, 
+				$this->headers, 
+				JSON_PRETTY_PRINT)->withHeaders($this->headers);
+	}	
+
+	/**
+	| -----------------------------------------------
+	| Successful HTTP requests
+	| -----------------------------------------------
+	| 
+	| Standard response for successful HTTP requests
+	**/
+	public function ok($data){
+		$this->data = $data;
+		$this->code = 200;
+		return $this;
+	}
+
+	/**
+	|-------------------------------------------------
+	| Creation of a new resource
+	|--------------------------------------------------
+	|
+	| The request has been fulfilled, resulting in the creation of a new resource
+	**/
+
+	public function created($data){
+		$this->data = $data;
+		$this->code = 201;
+		return $this;
+	}
+
+	/**
+	| ---------------------------------------------------
+	| Client Errors
+	| ---------------------------------------------------
+	| The server cannot or will not process the request 
+	| due to an apparent client error (Validation errors for example)
+	**/
+	
+	public function badRequest($data){
+		$this->data = $data;
+		$this->code = 400;
+		return $this;
+	}
+
+	/**
+	| ---------------------------------------------------
+	| Unauthorized user
+	| ---------------------------------------------------
+	| The user does not have the necessary credentials.
+	| Can be used when the token is expired or not presented.
+	**/
+
+	public function unauthorized($data){
+		$this->data = $data;
+		$this->code = 401;
+		return $this;
+	}
+
+	/**
+	| ---------------------------------------------------
+	| Forbidden "Do not have enough permissions"
+	| ---------------------------------------------------
+	|
+	| The user might be logged in but does not have the necessary permissions for the resource.
+	**/
+
+	public function forbidden($data){
+		$this->data = $data;
+		$this->code = 403;
+		return $this;
+	}
+
+	/**
+	 * ------------------------------------------------
+	 * Page not found
+	 * -----------------------------------------------
+	 *
+	 * Page not found error 404!
+	 */
+	public function notFound(){
+		$this->data = '404 Not found!';
+		$this->code = 404;
+		return $this;
+	}
+}
---
+++ app/Http/Controllers/Api/ExampleController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ExampleController extends Controller
+{   
+    private $response;
+
+    public function __construct(){
+        $this->response = new \App\Beak\Response;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   
+        // Example of getting data
+        $data = ['what is lorem ipsum', 'lorem impsum dolor sit'];
+        // return data with status code 200
+        return $this->response->ok($data)->respond();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+     /**
+     * Validate the given request with the given rules.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $rules
+     * @param  array  $messages
+     * @param  array  $customAttributes
+     * @return void
+     */
+    public function validate(Request $request, array $rules, array $messages = [], array $customAttributes = []){
+
+    }
+}
