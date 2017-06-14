04032bf0fdf3a0881b5f76404f1aa7545209b044
Mohamad Eldhemy <m.dhemy@outlook.com>
2017-06-15 00:31:44 +0200

    Created Files Table Seeder

--- database/seeds/FilesTableSeeder.php
+++ database/seeds/FilesTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Beak\Upload;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class FilesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(FilesTableSeeder::class);
+        
     }
 }
