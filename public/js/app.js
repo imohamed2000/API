b7fd01ce1b5fd10e003842ef61ae5aec03fe5aaf
Mohamad Eldhemy <m.dhemy@outlook.com>
2017-06-18 06:14:03 +0200

    Submit login form on Enter

--- public/js/app.js
+++ public/js/app.js
@@ -29643,7 +29643,14 @@ if (false) {
 module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
   return _c('form', {
     staticClass: "login-form",
+    attrs: {
+      "novalidate": ""
+    },
     on: {
+      "keyup": function($event) {
+        if (!('button' in $event) && _vm._k($event.keyCode, "enter", 13)) { return null; }
+        _vm.onSubmit($event)
+      },
       "submit": function($event) {
         $event.preventDefault();
         _vm.onSubmit($event)
@@ -29804,9 +29811,8 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
   }, [_vm._v(_vm._s(_vm.$t('Forgot Password?')))])]), _vm._v(" "), _c('button', {
     staticClass: "btn green mt-ladda-btn ladda-button",
     attrs: {
-      "id": "#submit",
-      "data-style": "zoom-in",
       "type": "submit",
+      "data-style": "zoom-in",
       "disabled": _vm.errors.any()
     }
   }, [_vm._v(_vm._s(_vm.$t('Sign In')))])])])])
