da7c1dc2506ab087b4a552b61360630b5d5bad89
Mohamad Eldhemy <m.dhemy@outlook.com>
2017-07-04 03:05:20 +0200

    Datatables with zero config

--- resources/assets/js/components/Schools.vue
+++ resources/assets/js/components/Schools.vue
@@ -1,10 +1,14 @@
 <template>
-	
+	<datatable></datatable>
 </template>
 <script>
 import {mapActions} from 'vuex';
+import datatable from './datatable';
 
 export default{
+	components:{
+		datatable: datatable
+	},
 	created(){
 		this.fetchData();
 	},
