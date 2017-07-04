30a318d0c798e96db88ace9c63407a81a632f714
Mohamad Eldhemy <m.dhemy@outlook.com>
2017-07-04 23:10:53 +0200

    Basic Datatables props and options

--- resources/assets/js/components/Schools.vue
+++ resources/assets/js/components/Schools.vue
@@ -1,11 +1,33 @@
 <template>
-	<datatable @rowClick="rowClick"></datatable>
+	<datatable @rowClick="rowClick" :props="props"></datatable>
 </template>
 <script>
 import {mapActions} from 'vuex';
 import datatable from './datatable';
 
 export default{
+	data(){
+		return{
+			props: {
+				class: 'table table-striped table-bordered table-hover dt-responsive',
+				width: '100%',
+				ajax: {
+					"url": "/api/v1/schools?datatables",
+                    "dataSrc": "original.data"
+				},
+				columns: [
+					{"data" : "name"},
+					{"data" : "email"},
+					{"data" : "city"},
+				],
+				headers: [
+					{'title' : 'Name', 'class': 'all'},
+					{'title' : 'Email', 'class': 'min-phone-l'},
+					{'title' : 'City', 'class': 'min-tablet'},
+				],
+			}
+		}
+	},
 	components:{
 		datatable: datatable
 	},
