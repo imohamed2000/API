0309be61a1530c8b43684025e7ffe1e5f90d0443
Mohamad Eldhemy <m.dhemy@outlook.com>
2017-07-08 12:22:56 +0200

    Created a portlet component

---
+++ resources/assets/js/components/Portlet.vue
@@ -0,0 +1,26 @@
+<template>
+	<div :class="props.class">
+
+		<div class="portlet-title">
+            <div class="caption font-dark" v-if="props.title">
+                <i :class="props.icon + ' ' + 'font-dark'"></i>
+                <span class="caption-subject bold uppercase" v-text="props.title"></span>
+            </div>
+            <div class="tools"><slot name="tools"></slot></div>
+        </div>
+		
+		<div class="portlet-body">
+			<slot name="body"></slot>
+		</div>
+
+		<div class="portlet-footer">
+			<slot name="footer"></slot>
+		</div>
+	</div>
+
+</template>
+<script>
+export default{
+	props: ['props'],
+}
+</script>
