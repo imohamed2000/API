174b10ee607db1c054fd0f0b9c1eb64e494c8a56
a-elwafa <eslamaboelwafa@gmail.com>
2017-07-09 09:15:51 +0200

    add slug to migration

--- database/migrations/2017_06_03_194133_create_schools_table.php
+++ database/migrations/2017_06_03_194133_create_schools_table.php
@@ -17,6 +17,7 @@ class CreateSchoolsTable extends Migration
 
             $table->increments('id');
             $table->string('name');
+            $table->string('slug')->unique();
             $table->string('email')->nullable();
             $table->string('address')->nullable();
             $table->string('city')->nullable();
