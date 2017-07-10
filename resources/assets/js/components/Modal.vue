afb970fe2ffe561200e35bedc9a9877404b15e28
Mohamad Eldhemy <m.dhemy@outlook.com>
2017-07-10 11:53:26 +0200

    Implemented Modal close button

--- resources/assets/js/components/Modal.vue
+++ resources/assets/js/components/Modal.vue
@@ -3,7 +3,7 @@
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<button @click="props.show = false" type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<slot name="modal-title"></slot>
 				</div>
 				<div class="modal-body">
