ff5a2fa3bdc7dcfd38541aefaf5835117a37ca29
Mohamad Eldhemy <m.dhemy@outlook.com>
2017-07-10 15:53:09 +0200

    Implemented different modal sizes

--- resources/assets/js/components/Modal.vue
+++ resources/assets/js/components/Modal.vue
@@ -1,17 +1,12 @@
 <template>
-	<div class="modal fade in" v-if="props.show" style="display:block">
-		<div class="modal-dialog">
+	<div class="modal fade in" tabindex="-1" aria-hidden="true" v-if="props.show" style="display:block">
+		<div :class="size">
 			<div class="modal-content">
 				<div class="modal-header">
 					<button @click="props.show = false" type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<slot name="modal-title"></slot>
 				</div>
-				<div class="modal-body">
-					<slot name="modal-body"></slot>
-				</div>
-				<div class="modal-footer">
-					<slot name="modal-footer"></slot>
-				</div>
+				<slot name="modal-content"></slot>
 			</div>
 		</div>
 	</div>
@@ -19,5 +14,16 @@
 <script>
 export default{
 	props: ['props'],
+	computed: {
+		size: function(){
+			let sizes = {
+				basic: 'modal-dialog',
+				full: 'modal-dialog modal-full',
+				large: 'modal-dialog modal-lg',
+				small: 'modal-dialog modal-sm'
+			};
+			return sizes[this.props.size];
+		}
+	}
 }
 </script>
