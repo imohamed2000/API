33a86d00e47093e0a7bf41723138fef53ff2f32e
Mohamad Eldhemy <m.dhemy@outlook.com>
2017-08-07 20:53:14 +0200

    Added route to school show

--- resources/assets/js/router/routes.js
+++ resources/assets/js/router/routes.js
@@ -1,15 +1,18 @@
 module.exports = [
+	// Entry page
 	{path: '/', component: require('../components/Stats'), name: 'stats',
 		 meta: {
 		 	title: "Home",
 		 	pageTitle: "Dashboard"
 	 }},
-
-	{path: '/schools', component: require('../components/SchoolsIndex'), name: 'schools',
+	 // Schools Index page
+	{path: '/schools', component: require('../components/SchoolsIndex'), name: 'schoolsIndex',
 		meta: {
 				title: "All Schools",
 				pageTitle: "All Schools"
 	}},
-
+	// Schools Show page
+	{path: '/schools/:slug', component: require('../components/SchoolsShow'), name: 'schoolsShow'},
+	// 404 Page
 	{path: '*', component: require('../components/NotFound'), name: '404', meta: {title: 'Error 404'}}
 ];
