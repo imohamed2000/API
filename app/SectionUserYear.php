d5daff35e72d34b58a90293f5d42c34a895aa065
a-elwafa <eslamaboelwafa@gmail.com>
2017-09-04 22:05:46 +0200

    Modified with users relation

--- app/SectionUserYear.php
+++ app/SectionUserYear.php
@@ -16,4 +16,9 @@ class SectionUserYear extends Model
     {
         return $this->belongsTo('App\Section','section_id');
     }
+
+    public function users()
+    {
+        return $this->belongsTo('App\User','user_id');
+    }
 }
