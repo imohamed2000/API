37336e47fb3a41671186ce58f1875c9142561182
a-elwafa <eslamaboelwafa@gmail.com>
2017-09-04 20:41:48 +0200

    Create mapping model

---
+++ app/SectionUserYear.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SectionUserYear extends Model
+{
+    protected $table = 'section_user_year';
+
+    public $timestamps = false;
+
+    protected $guarded = [];
+}
