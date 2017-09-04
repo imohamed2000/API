d671ff19b9d452a91a076cc6edff80d4c27c6f94
a-elwafa <eslamaboelwafa@gmail.com>
2017-09-04 20:39:54 +0200

    Modify UserController with Store maping

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,30 +9,26 @@ use App\Beak\Upload;
 
 class UserController extends Controller
 {
-    private $list = ['users.id','first_name', 'last_name','email'];
-    private $default_avatars = [1,2];
+    private $list = ['users.id', 'first_name', 'last_name', 'email'];
+    private $default_avatars = [1, 2];
 
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request,School $school)
-    {   
+    public function index(Request $request, School $school)
+    {
         $data = $school->users()->latest()->select($this->list);
-        if( $request->exists('datatables') )
-        {
+        if ($request->exists('datatables')) {
             return $this->response
-                ->dataTables( $data->get() )
+                ->dataTables($data->get())
                 ->respond();
         }
 
-        if( $request->has('per_page') )
-        {
-            $data = $data->paginate( $request->input('per_page') );
-        }
-        else
-        {
+        if ($request->has('per_page')) {
+            $data = $data->paginate($request->input('per_page'));
+        } else {
             $data = $data->get();
         }
 
@@ -52,63 +48,57 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request,School $school)
+    public function store(Request $request, School $school)
     {
-        $is_valid = $this->validate($request->all(),[
-            'role'              => 'required|exists:roles,id', // TODO validate that the role is connected to the current school
-            'title'             => 'max:10',
-            'first_name'        => 'required|max:255',
-            'last_name'         => 'required|max:255',
-            'birth_date'        => 'date|nullable',
-            'phone'             => 'max:42',
-            'address'           => 'max:255',
-            'gender'            => 'required|in:male,female',
-            'email'             => 'required|email|unique:users,email',
-            'password'          => 'required|min:6',
-            'avatar'            => 'image|nullable'
+        $is_valid = $this->validate($request->all(), [
+            'role' => 'required|exists:roles,id', // TODO validate that the role is connected to the current school
+            'title' => 'max:10',
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'birth_date' => 'date|nullable',
+            'phone' => 'max:42',
+            'address' => 'max:255',
+            'gender' => 'required|in:male,female',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'avatar' => 'image|nullable'
         ]);
 
-        if(!$is_valid)
-        {
+        if (!$is_valid) {
             return $this->response->badRequest($this->errors)->respond();
         }
-        if($request->hasFile('avatar'))
-        {   
+        if ($request->hasFile('avatar')) {
             $avatar = new Upload;
-            $avatar->put( $request->file('avatar'), 'users/avatar' );
+            $avatar->put($request->file('avatar'), 'users/avatar');
             $avatar = $avatar->getFileData()->id;
-        }
-        else
-        {
-            if($request->gender === 'male'){
+        } else {
+            if ($request->gender === 'male') {
                 $avatar = 1;
-            }
-            else
-            {
+            } else {
                 $avatar = 2;
             }
         }
         $attr = [
-            'title'             => $request->title,
-            'first_name'        => $request->first_name,
-            'last_name'         => $request->last_name,
-            'birth_date'        => $request->birth_date,
-            'phone'             => $request->phone,
-            'address'           => $request->address,
-            'gender'            => $request->gender,
-            'email'             => $request->email,
-            'password'          => bcrypt($request->password),
-            'avatar'            => $avatar
+            'title' => $request->title,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'birth_date' => $request->birth_date,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'gender' => $request->gender,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'avatar' => $avatar
         ];
 
         $user = $school->users()->create($attr);
 
         // save User Role
-        $role = \App\Role::find( $request->role );
-        $user->roles()->save( $role );
+        $role = \App\Role::find($request->role);
+        $user->roles()->save($role);
 
         return $this->response->created($user)->respond();
 
@@ -117,11 +107,11 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function show(School $school,$id)
-    {   
+    public function show(School $school, $id)
+    {
         $data = $school->users()->with('roles')->findOrFail($id);
         return $this->response->ok($data)->respond();
     }
@@ -129,7 +119,7 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -140,65 +130,64 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,School $school, $id)
+    public function update(Request $request, School $school, $id)
     {
         $user = $school->users()->findOrFail($id);
-        $is_valid = $this->validate($request->all(),[
-            'role'              => 'required|exists:roles,id', // TODO validate that the role is connected to the current school
-            'title'             => 'max:10',
-            'first_name'        => 'required|max:255',
-            'last_name'         => 'required|max:255',
-            'birth_date'        => 'date|nullable',
-            'phone'             => 'max:42',
-            'address'           => 'max:255',
-            'gender'            => 'required|in:male,female',
-            'email'             => 'required|email|unique:users,email,' . $user->id,
-            'avatar'            => 'image|nullable'
+        $is_valid = $this->validate($request->all(), [
+            'role' => 'required|exists:roles,id', // TODO validate that the role is connected to the current school
+            'title' => 'max:10',
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'birth_date' => 'date|nullable',
+            'phone' => 'max:42',
+            'address' => 'max:255',
+            'gender' => 'required|in:male,female',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+            'avatar' => 'image|nullable'
         ]);
 
-        if(!$is_valid)
-        {
+        if (!$is_valid) {
             return $this->response->badRequest($this->errors)->respond();
         }
 
-        $user->title        = $request->title;
-        $user->first_name   = $request->first_name;
-        $user->last_name    = $request->last_name;
-        $user->birth_date   = $request->birth_date;
-        $user->phone        = $request->phone;
-        $user->address      = $request->address;
-        $user->gender       = $request->gender;
-       
-       // Updating Password
-       if($request->has('password')){
-            $user->password = bcrypt( $request->input('password') );
-       }
-       // Updating Avatar
-       if($request->hasFile('avatar')){
+        $user->title = $request->title;
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->birth_date = $request->birth_date;
+        $user->phone = $request->phone;
+        $user->address = $request->address;
+        $user->gender = $request->gender;
+
+        // Updating Password
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->input('password'));
+        }
+        // Updating Avatar
+        if ($request->hasFile('avatar')) {
             $avatar = new Upload;
-            if( in_array($user->avatar, $this->default_avatars) ){
+            if (in_array($user->avatar, $this->default_avatars)) {
                 // Upload new avatar
-                $avatar->put( $request->file('avatar'), 'users/avatar' );
-            }else{
+                $avatar->put($request->file('avatar'), 'users/avatar');
+            } else {
                 // Override old avatar
-                $avatar->replace( $user->avatar, $request->file('avatar') );
+                $avatar->replace($user->avatar, $request->file('avatar'));
             }
             $user->avatar = $avatar->getFileData()->id;
-       }
-       // Clearing Avatar
-       if($request->has('clear_avatar')){
+        }
+        // Clearing Avatar
+        if ($request->has('clear_avatar')) {
             $avatar = $user->gender == 'male' ? $this->default_avatars[0] : $this->default_avatars[1];
             $user->avatar = $avatar;
-       }
-       // Update User Role
-       if($user->role != $request->input('role')){
-        $user->roles()->sync([$request->input('role')]);
-       }
-        
+        }
+        // Update User Role
+        if ($user->role != $request->input('role')) {
+            $user->roles()->sync([$request->input('role')]);
+        }
+
         $user->save();
         return $this->response->ok($user)->respond();
 
@@ -207,10 +196,10 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(School $school,$id)
+    public function destroy(School $school, $id)
     {
         $user = $school->users()->findOrFail($id);
         $user->delete();
@@ -227,20 +216,48 @@ class UserController extends Controller
         $trashed = $school->users()->onlyTrashed()->get();
         return $this->response->ok($trashed)->respond();
     }
+
     /**
      * Restore  specific trashed
-     * @param  \App\School  $school
+     * @param  \App\School $school
      * @param int $id
      * @return \App\Beak\Response
      */
-    public function restore(School $school,$id)
+    public function restore(School $school, $id)
     {
         $user = $school->users()->findOrFail($id);
-        if ($user->trashed())
-        {
+        if ($user->trashed()) {
             $user->restore();
             return $this->response->ok($user)->respond();
         }
         return $this->response->badRequest(['Error request'])->respond();
+    }
+
+
+    public function storeSections(Request $request, School $school, $id)
+    {
+        $is_valid = $this->validate($request->all(), [
+            'section' => 'required|exists:sections,id',
+            'year' => 'required|exists:sections,id',
+        ]);
+        if (!$is_valid) {
+            return $this->response->badRequest($this->errors)->respond();
+        }
+        $checkUser = $school->checkUser($school->id,$id);
+        if(!$checkUser){
+            return $this->response->badRequest(['Sorry!, this user has no permission with this school'])->respond();
+        }
+        $checkSection = $school->checkSection($school->id,$request->section);
+        if(!$checkSection){
+            return $this->response->badRequest(['Sorry!, this section has no permission with this school'])->respond();
+        }
+        $checkYear = $school->checkYear($school->id,$request->year);
+        if(!$checkYear){
+            return $this->response->badRequest(['Sorry!, this year has no permission with this school'])->respond();
+        }
+
+        \App\SectionUserYear::create(['user_id'=>$id , 'year_id'=>$request->year,'section_id'=>$request->section]);
+
+        return $this->response->created(['saved'])->respond();
     }
 }
