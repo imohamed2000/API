059464fac58d16f0ebd133b301889c2055bad5dd
a-elwafa <eslamaboelwafa@gmail.com>
2017-09-05 18:27:43 +0200

    Modify with new routes for get current and post current

--- routes/api.php
+++ routes/api.php
@@ -28,8 +28,7 @@ Route::group(['prefix' => 'v1'], function () {
     Route::resource('schools','SchoolsController');
     Route::get('schools/trashed','SchoolsController@trashed');
     Route::post('schools/restore/{id}','SchoolsController@restore');
-    Route::get('school/current/{id}','SchoolsController@getCurrent');
-    Route::post('school/{school}/current/{id}','SchoolsController@postCurrent');
+    Route::get('school/{school}/year','SchoolsController@getCurrent');
     Route::resource('school.users', 'UserController');
     Route::get('school/{School}/users/trashed','UserController@trashed');
     Route::post('school/{school}/users/{user}/restore','UserController@restore');
@@ -45,6 +44,7 @@ Route::group(['prefix' => 'v1'], function () {
     Route::resource('school.roles', 'RolesController');
     Route::apiResource('school.grades', 'GradeController');
     Route::apiResource('school.years', 'YearController');
+    Route::post('school/{school}/year/{year}/active','YearController@postCurrent');
     Route::post('school/{school}/grades/sort', 'GradeController@sort');
     Route::get('school/{School}/grades/trashed','GradeController@trashed');
     Route::post('school/{school}/grades/{grade}/restore','GradeController@restore');
