efb7296b1e20b85a47c7af49df1320e93b1811bc
a-elwafa <eslamaboelwafa@gmail.com>
2017-09-11 13:58:56 +0200

    Implement users relation and Implement scopeFilter method

--- app/Role.php
+++ app/Role.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Beak\QueryFilter;
 
 class Role extends Model
 {
@@ -14,5 +15,15 @@ class Role extends Model
     public function permissions()
     {
         return $this->belongsToMany('App\Permission','roles_permissions');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User', 'role_user', 'role_id', 'user_id');
+    }
+
+    public function scopeFilter($query, QueryFilter $filters)
+    {
+        return $filters->apply($query);
     }
 }
