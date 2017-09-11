67022f956b12efc65f801e0c4dfe009e7cbb424d
a-elwafa <eslamaboelwafa@gmail.com>
2017-09-11 13:56:55 +0200

    Implement RoleFilter class

---
+++ app/Beak/RoleFilter.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Beak;
+
+class RoleFilter extends QueryFilter
+{
+    public function role($name)
+    {
+        return $this->builder->where('name','LIKE','%'.$name.'%');
+    }
+}
